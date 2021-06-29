d00f7770688cff7530fbd023b85f03dfd973b4c7
thezline <juan.bajonero.gdcan@gmail.com>
2021-06-29 12:05:44 -0700

    Demo turned into class

--- intelli/demo/index.py
+++ intelli/demo/index.py
@@ -5,15 +5,21 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import sentiment
 from nltk import word_tokenize
 
-#f = open("../demo/text.txt", "r")
-f = open("text.txt", "r")
-analizer = SentimentIntensityAnalyzer()
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-sentences = tokenizer.tokenize(f.read())
+class Sentiment_Analyzer_Demo:
+    """ Demo class """
 
-for sentence in sentences:
-    print(sentence)
-    scores = analizer.polarity_scores(sentence)
-    for key in scores:
-        print(key, ':', scores[key])
-        print()
+    def get_sentiment(self):
+        f = open("text.txt", "r")
+        analizer = SentimentIntensityAnalyzer()
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        sentences = tokenizer.tokenize(f.read())
+
+        for sentence in sentences:
+            print(sentence)
+            scores = analizer.polarity_scores(sentence)
+            for key in scores:
+                print(key, ':', scores[key])
+            print()
+
+sentimentAnalizerDemo = Sentiment_Analyzer_Demo()
+print(sentimentAnalizerDemo.get_sentiment())
