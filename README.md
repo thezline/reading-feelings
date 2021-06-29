b64278cd17e80f81aacd0ae315d4724feb54ab9d
thezline <juan.bajonero.gdcan@gmail.com>
2021-06-28 17:14:52 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Reading feelings (RF Project)
+
+###Description
+Artificial Intelligence project in conjunction with the Flask framework. Focused on open-ended questions in surveys, RF Project analyzes if the questions that the user fills out contain a positive, negative or neutral connotation.
+
+###Technologies
+                    
+Name  | Version
+------------- | -------------
+Python  | 3.9.6
+Flask  | 2.0.1
+Nltk library  | 3.6.2
+
+###Test only the AI
+In the root dir, go to:
+>cd /intelli/demo/
+
+Execute:
+>python3 index.py
---
+++ intelli/index.py
@@ -0,0 +1,19 @@
+import nltk
+#nltk.download('vader_lexicon')
+#nltk.download('punkt')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import sentiment
+from nltk import word_tokenize
+
+#f = open("../demo/text.txt", "r")
+f = open("../demo/text.txt", "r")
+analizer = SentimentIntensityAnalyzer()
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+sentences = tokenizer.tokenize(f.read())
+
+for sentence in sentences:
+    print(sentence)
+    scores = analizer.polarity_scores(sentence)
+    for key in scores:
+        print(key, ':', scores[key])
+        print()
