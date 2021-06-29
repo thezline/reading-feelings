85dbe997735caa913d92864b3b740eb5b7b7b803
thezline <juan.bajonero.gdcan@gmail.com>
2021-06-29 10:43:12 -0700

    Home post created for the survey form

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('home.html')
-   
+
+@app.route('/', methods=['POST'])
+def home_post():
+    firstQuestion = request.form['firstQuestion']
+    secondQuestion = request.form['secondQuestion']
+    thirdQuestion = request.form['thirdQuestion']
+    thirdQuestion = request.form['thirdQuestion']
+    openEndedQuestion = request.form['openEndedQuestion']
+
+    return openEndedQuestion
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
